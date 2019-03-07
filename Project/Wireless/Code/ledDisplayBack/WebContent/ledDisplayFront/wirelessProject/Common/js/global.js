74a0653fb54cc1719c974577aa4de7f8e934ab11
lsy <594020462@qq.com>
2019-03-07 09:55:05 +0800

    global更新

--- Project/Wireless/Code/ledDisplayBack/WebContent/ledDisplayFront/wirelessProject/Common/js/global.js
+++ Project/Wireless/Code/ledDisplayBack/WebContent/ledDisplayFront/wirelessProject/Common/js/global.js
@@ -1,41 +1,45 @@
-var env = 0, //0本机 1生产
-    apiurlArr = ['http://10.1.4.194:8080/ledDisplayBack/', 'http://10.9.3.20:8080/ledDisplayBack/'],
-    g={};
-
-    g.doAjax = function (url,_data,func,sync, cache, isGet ) { //封装ajax的一些常用参数  //data数据可以为空
-        // _data = $.extend({}, {
-        //     "ASYNC": true,
-        //     "head": {"token": ""},
-        //     // "body":_data
-        // } ,_data); //默认参数
-        // console.log(_data,11)
-        $.ajax({
-            // cache: cache,
-            // type: isGet ? "GET" : "POST",
-            // dataType: "JSON",
-            // jsonp: 'callback',
-            // jsonpCallback: 'jsonpCallback',
-            dataType: 'json',   
-            type: 'POST',
-            async: true,//异步请求
-            url: apiurlArr[env]+url,
-            data:_data,
-            success: function (res) {
+var env = 1, //0本机 1生产
+    apiurlArr = ['http://10.4.123.255:8080/ledDisplayBack/', 'http://10.9.3.20:8080/ledDisplayBack/'],
+    g={
+        doAjax : function (url,_data,func,sync, cache, isGet ) { //封装ajax的一些常用参数  //data数据可以为空
+            // _data = $.extend({}, {
+            //     "ASYNC": true,
+            //     "head": {"token": ""},
+            //     // "body":_data
+            // } ,_data); //默认参数
+            // console.log(_data,11)
+            $.ajax({
+                // cache: cache,
+                // type: isGet ? "GET" : "POST",
+                // dataType: "JSON",
+                // jsonp: 'callback',
+                // jsonpCallback: 'jsonpCallback',
+                dataType: 'json',   
+                type: 'POST',
+                async: true,//异步请求
+                url: apiurlArr[env]+url,
+                data:_data,
+                success: function (res) {
+                    
+                    func(res);
+                },
+                beforSend: function () {
+                    // 禁用按钮防止重复提交
+                    // $("#submit").attr({disabled: "disabled"});
+                },
+                error: function (res) {
+                    //请求失败时被调用的函数 
+                    // alert(JSON.stringify(msg));
+                    console.log(res,"error");
+                }
                 
-                func(res);
-            },
-            beforSend: function () {
-                // 禁用按钮防止重复提交
-                // $("#submit").attr({disabled: "disabled"});
-            },
-            error: function (res) {
-                //请求失败时被调用的函数 
-                // alert(JSON.stringify(msg));
-                console.log(res,"error");
-            }
-            
-        });
-       
-        return func;
-    }
+            });
+           
+            return func;
+        },
+        dataFresh : function (target) {
+            $(target).counterUp({ delay: 100, time: 2000 });
+        }
+
+    };
 
